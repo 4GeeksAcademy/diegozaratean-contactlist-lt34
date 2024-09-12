c3f67867277f987cb82103d04bee9a0f69a9694f
diego zarate <diegozaratean@gmail.com>
2024-09-12 01:43:53 +0000

    context part 1

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {useContext} from "react";
+import { Context } from "../store/appContext";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+
+	const { actions } = useContext(Context)
+
+	function changeTitle(){
+		console.log('changeTitle')
+		actions.changeMessage()
+	}
+
+
+	return(
+		<div className="text-center mt-5">
+			<h1>Hello Rigo!</h1>
+			<button onClick={actions.changeMessage} >Cambiar saludo</button>
+			<p>
+				<img src={rigoImage} />
+			</p>
+			<a href="#" className="btn btn-success">
+				If you see this green button, bootstrap is working
+			</a>
+		</div>
+	);
+} 
